b417a372328771a234abd172a70debecc3435386
Rainacam <rainacam12@gmail.com>
2023-06-12 13:50:17 -0400

    class instructions followed

---
+++ src/components/Crystal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Crystal = () => {
+    return (
+        <li>
+            <h2>Crystal Name</h2>
+            <p>Crystal Color</p>    
+            <p>Crystal Powers</p> 
+            <button>Charge</button>   
+        </li>
+    )
+}
+
+export default Crystal
